e590358744bbb50c4244fae1b1b8bfe4ccdf54b4
fomazov <oleksii@fomazov.name>
2023-12-04 13:15:28 +0200

    chore: Add shadcd/ui (reusable UI components)

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,18 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
+// Fonts
+import { Inter as FontSans } from "next/font/google";
+
+// Styles
+import "@/styles/globals.css";
+
+// Helpers
+import { cn } from "@/lib/utils";
+
+export const fontSans = FontSans({
+  subsets: ["latin"],
+  variable: "--font-sans",
+});
 
 export const metadata: Metadata = {
   title: "Deep Dive Tech",
@@ -16,7 +26,14 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body
+        className={cn(
+          "min-h-screen bg-background font-sans antialiased",
+          fontSans.variable
+        )}
+      >
+        {children}
+      </body>
     </html>
   );
 }
